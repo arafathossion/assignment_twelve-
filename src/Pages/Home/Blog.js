580c3fbe08e870e9b0f1312af38ef26adedc4785
arafathossion <arafathossionrahman@gmail.com>
2022-05-26 19:39:23 +0600

    blog

---
+++ src/Pages/Home/Blog.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from 'react';
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import "./styles.css";
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper";
+import { Link } from 'react-router-dom';
+
+const Blog = () => {
+    const [blogs, setBlogs] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/blogs')
+            .then(res => res.json())
+            .then(data => setBlogs(data))
+    }, []);
+    console.log(blogs)
+    return (
+        <div className='px-16'>
+
+            <div className='text-center pb-20'>
+                <h1 className='text-3xl font-extrabold capitalize'>Millons business trust us</h1>
+                <h4 className='text-3xl font-medium uppercase py-4 text-teal-500'>our blog</h4>
+            </div>
+
+
+
+            <Swiper
+                slidesPerView={3}
+                spaceBetween={30}
+                autoplay={{
+                    delay: 3000,
+                    disableOnInteraction: false,
+                    pauseOnMouseEnter: true
+                }}
+                pagination={{
+                    clickable: true
+                }}
+                navigation={true}
+                modules={[Autoplay, Pagination, Navigation]}
+                className="my-custom-style h-auto"
+            >
+                {
+                    blogs?.map(blog => <SwiperSlide className='h-auto'>
+                        <div class="card card-compact my-custom-style">
+                            <figure><img src="https://api.lorem.space/image/shoes?w=400&h=225" alt="Shoes" className='p-4' /></figure>
+                            <div class="card-body">
+                                <h2 class="card-title">{blog?.author}</h2>
+                                <h2 class="text-md font-medium">{blog?.question.slice(0, 50)} ....</h2>
+                                <p>{blog?.short_description.slice(0, 200)} ....</p>
+                                {/*
+                                {
+                                   
+                                } */}
+                                <div class="card-actions justify-end">
+                                    <Link to={`/blog/${blog?._id}`} class="btn btn-primary">Buy Now</Link>
+                                </div>
+                            </div>
+                        </div>
+                    </SwiperSlide>)
+                }
+            </Swiper>
+
+        </div>
+    );
+};
+
+export default Blog;
